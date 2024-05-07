da71cd93d5eefc209d46b8d599322229bf45a75e
Shashank <96738298+ShashankKotla@users.noreply.github.com>
2024-05-07 08:33:15 +0000

    learned upto conversions

--- JS/01_variable.js
+++ JS/01_variable.js
@@ -1,1 +1,38 @@
-console.log('Common sence')
+// const accountId = 133455;
+// let accountEmail = 'test@test.com';
+// var accountNum = 54; //var can't be used, becoz of it's scope acknowledge
+// let state;
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountNum);
+
+// console.table([accountId, accountEmail, accountNum, state]);
+ 
+"use strict"; // treat as newer version.
+
+//alert(), can be used in browser, not in node engine.
+
+// Datatypes :-
+// primitive -> number (2 pow 53), bigint, string, boolean, null (stanalone value), undefined (value not assigned), symbol (unique, most used in react)
+
+// console.log(typeof Symbol); //function
+//num ->obj
+//undefined will be undefined
+
+// Conversion Operation :-
+
+//Note : null -0, true-1, undefined-NaN, 'h3'-NaN
+
+// let value = undefined
+
+// let value2 = Number(value)
+// console.log(typeof value2);
+// console.log(value2);
+
+// let loggedIn = ''
+// // ' '-true.''-false, 1-true,0-false
+// let loggedIn2 = Boolean(loggedIn)
+// console.log(typeof loggedIn2);
+// console.log(loggedIn2);
+
