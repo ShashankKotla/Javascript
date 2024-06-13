c712f9c37c4472efa69122e47af3b236052f962c
ShashankKotla <shashankkotla50@gmail.com>
2024-06-13 21:05:20 +0530

    progressing on js learning for the finial time

--- finial_turn.js
+++ finial_turn.js
@@ -1,1 +1,38 @@
-console.log("Hello");
+// console.log("Hello");
+
+// console.log(1<<2.34);
+
+// console.log((1+3) * 2);
+
+// Not-real-number
+// console.log(typeof(Infinity), '->',Infinity);
+// console.log(typeof(-Infinity), '->',-Infinity);
+// console.log(typeof(NaN), '->',NaN);
+
+// console.log(`true : ${(true)}, false: ${false}`);
+// console.log(`Negation'!'->true : ${(!true)}, false: ${!false}`);
+
+// console.log(1===1);
+// console.log(1!==1);
+// console.log(2===1);
+// console.log(2!==1);
+
+// String
+
+// console.log("Hello" + 3);
+// console.log("Hello" + ["world", "!"]);
+
+// console.log('5'===5);
+// console.log(null===undefined);
+
+// console.log('India is my country'.charAt(0)); //to access a  single character
+
+// console.log("India is my country".substring(0,5));//get multi characters
+// console.log("India".length);
+
+// console.log(typeof null); //it's an object & indicates a deliberate non-value
+
+// console.log(typeof undefined); //it's an value 
+
+// var test; //undefined
+
