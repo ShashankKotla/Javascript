b280fd3493bfe7485d786a3f9336b6d17fa6d92d
Shashank <96738298+ShashankKotla@users.noreply.github.com>
2024-06-21 15:40:41 +0530

    work with html
    Learning JS with html.

---
+++ code/intro/jsW3.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById('demo').innerHTML= "Hacked !!!"
+}
+
+/*
+Javascript Keywords
+
+var -> Declares a variable
+let -> Declares a block variable
+const -> Declares a block constant
+if -> Marks a block of statements to be executed on a condition.
+switch -> Marks a block of statements to be executed in different cases.
+for -> Marks a block of statements to be executed in different cases.
+function -> Declares a function.
+return -> Exists a function.
+try -> Implements error handling to a block of statements.
+*/
--- finial_turn.js
+++ finial_turn.js
@@ -36,3 +36,52 @@
 
 // var test; //undefined
 
+let someVar = 5;
+// let someVarr = someVar++;
+// console.log(someVarr);
+// console.log(someVar);
+
+// let someVar4 = 5
+// let someVar5 = ++someVar4;
+// console.log(someVar5);
+// console.log(someVar4);
+
+// let someVar2 = 10;
+// console.log(someVar2 *= 10);
+// someVar++
+// someVar--
+// console.log(someVar);
+
+var myArray = ['Hello', 45, true]
+// console.log(myArray[1]);
+
+// myArray.push('world')
+// console.log(myArray.length);
+// myArray[1] = 54;
+
+// myArray.unshift(3) //add as the first element
+// console.log(myArray);
+
+// someVar = myArray.shift() //remove first element & return it
+// myArray.pop() //removes last element
+// console.log(myArray);
+
+// let test = [32, false, "js", 12, 56, 90];
+
+// console.log(test.join(";"));
+
+// console.log(test.slice(2)); //get subarray of elements
+
+// test.splice(1,3, "Time", "is", "important")
+// test.splice(2,2, "hi", "Bye", "Ok", "JS")
+// console.log(test);
+
+
+
+// let myObj ={
+//     Key1 : "value1",
+//     key2 : "value2"
+// }
+
+
+
