cdaf132bf26b6f04efd905161782f4bad31ad7be
Shashank <96738298+ShashankKotla@users.noreply.github.com>
2024-06-24 18:45:41 +0530

    Learnt on Operators and Related pages
    Learnt up to operators and related linked pages like Date get methods and some random topics

--- code/intro/jsW3.js
+++ code/intro/jsW3.js
@@ -102,3 +102,106 @@ $ sign, is not very common in js. but professional programmers often use it as a
 // console.log('2'>'12');
 // console.log("2" == "12");
 
+// Javascript Type Conversion
+
+// 1. Converting strings to Numbers
+// console.log(Number(Math.PI));
+// console.log(Number('3.14'));
+// console.log(Number("")); //returns 0
+// console.log(Number("John")); //returns NaN
+
+// let x = 123;
+// Number methods
+// toString() method
+// console.log(x.toString());
+// console.log(typeof x.toString());
+// console.log((100 + 50).toString());
+
+// toExponential() method
+let x = 555.6;
+// console.log(x.toExponential(3)); //returns a number with exponential notation 'e' and rounded numbers.
+
+// toFixed() method
+// console.log(x.toFixed(2)); //returns a str, with the no. written with a specified number of decimals. (no.of 00 gonna added)
+
+// toPrecision, return a string, with a number written with a specified length.
+// console.log(x.toPrecision(3)); //return required length numbers like 564 if we provide parameter 6 then it will return 564.000
+// let z = 564
+// console.log(z.toPrecision(6)); 
+
+// valueOf(), returns number as number
+// console.log(x.valueOf());
+
+// Number() method on dates
+// console.log(Number(new Date('1970-01-02')));
+
+// parseInt(), returns a whole number. spaces are allowed(but only the first number is returned.)
+// console.log(parseInt("-10"));
+// console.log(parseInt("-10.33"));
+// console.log(parseInt("10"));
+// console.log(parseInt("10.33"));
+// console.log(parseInt("10 20 30"));//spaces are allowed, but only first number will be returned.
+// console.log(parseInt("10 years"));
+// console.log(parseInt("years 10"));
+
+// console.log(parseFloat("-10"));
+// console.log(parseFloat("-10.33"));
+// console.log(parseFloat("10"));
+// console.log(parseFloat("10.33"));
+// console.log(parseFloat("10 20 30"));//spaces are allowed, but only first number will be returned.
+// console.log(parseFloat("10 years"));
+// console.log(parseFloat("years 10"));
+
+// console.log(Number.isInteger(10));
+// console.log(Number.isInteger(10.5));
+// console.log(Number.isSafeInteger(10));
+
+
+// DATES
+const d = new Date();
+// console.log(d);
+// console.log(Number(d));
+// console.log(d.getTime());
+// console.log(String(Date())); //convert dates to string.->"Mon Jun 24 2024 17:27:20 GMT+0530 (India Standard Time)"
+// console.log(Date().toString());
+// console.log(d.getFullYear());
+// console.log(d.getMonth()); //0-11 way.
+// const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// let month = months[d.getMonth()];
+// console.log(month);
+
+// console.log(d.getDay()); // mon(1) =>1
+// console.log(d.getTime()); //o/p ->1719231753575
+
+// console.log(d.getDate());
+// const dat = new Date("2024-06-24");
+// console.log(dat.getDate()); //25
+// console.log(dat.getMinutes());
+// console.log(dat.getSeconds());
+// console.log(dat.getMilliseconds());
+
+// console.log(d.getHours());
+// console.log(`${d.getHours()} : ${d.getMinutes()}`);
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
+// console.log(d.getMilliseconds());
+
+// console.log(d.getDay());
+// const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// console.log(days[d.getDay()]);
+
+// const d2 = new Date('2024-06-24');
+// const d2 = new Date();
+// console.log(d2.getTime()); //returns milliseconds since jan1,1970
+
+// console.log(d.getTimezoneOffset()); //returns difference (in minutes) b/w local an UTC time.
+
+// JavaScript Bitwise Operators
+//Bitwise operators work on 32 bit numbers.
+console.log(5 & 1); //& works on comparing each digit-> 0101 & 0001, here 1 matched both sides.
+console.log(5 | 1); //it is opposite ofo &
+console.log(~5); //~(Bitwise Not) -> inverts all bits 0101 to 1010 which is 6
+console.log(5 ^ 1);  //0101 ^ 0001 -> 0100
+console.log(5 << 1); //5x2 or we can say shift 1 bit to left 0101 to 1010 ->10
+console.log(5 >> 1); //0101 -> 0010 which is 2
+console.log(5 >>> 1); //unsigned right shift (it behaves same as signed right shift).
