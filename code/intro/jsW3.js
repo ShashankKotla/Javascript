8731354051dfe9ca39858d495687665638bf4a13
Shashank <96738298+ShashankKotla@users.noreply.github.com>
2024-06-24 14:30:49 +0530

    Worked on variables
    learnt about the difference b/w let, var and const.

--- code/intro/jsW3.js
+++ code/intro/jsW3.js
@@ -14,4 +14,30 @@ for -> Marks a block of statements to be executed in different cases.
 function -> Declares a function.
 return -> Exists a function.
 try -> Implements error handling to a block of statements.
+$ sign, is not very common in js. but professional programmers often use it as an alias for the main function in a js library.
+' _ ' underscore, is not very common in js, but a convention amoung professional programmers is to use it as an alias for 'private (hidden)' vairables.
 */
+
+
+// const PI = 3.14;
+//Note: const->It does not define a constant value.It defines a constant reference to a value. so you can change elements of constant array's & object's.
+// console.log(PI);
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[0] = "Toyota";
+// cars.push("Audi");
+// console.log(cars);
+
+// const car = {type: "Flat", model: "500", color: "White"};
+// car.color = 'red';
+// car.owner = 'John';
+// console.log(car);
+// {
+//     const car = {type: "test"}
+//     console.log(car);
+// }
+// {
+//     const car = {type: "testOne"}
+//     console.log(car);
+// }
+
