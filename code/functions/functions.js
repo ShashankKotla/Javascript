a59bc879f55da865d2d4b780414393cc1ea34d0d
Shashank <96738298+ShashankKotla@users.noreply.github.com>
2024-06-25 14:39:24 +0530

    learnt topic of functions

---
+++ code/functions/functions.js
@@ -0,0 +1,14 @@
+// A JS Function is a block of code designed to perform a particular task.
+// It works when it is invoked (calls it).
+
+
+// function myFuntion(p1, p2) {
+//     return p1 * p2;
+// }
+// document.getElementById('func').innerHTML=myFuntion(3,2);
+
+// function toCelsius(fahrenheit){
+//     return (5/9) * (fahrenheit-32);
+// }
+// document.getElementById('func').innerHTML= toCelsius(77);
+
