07062809ea58028e0ac70ee4e36d1104fc28a8e0
Shashank <96738298+ShashankKotla@users.noreply.github.com>
2024-06-25 13:53:53 +0530

    learnt about the datatypes

---
+++ code/DTypes/dataTypes.js
@@ -0,0 +1,58 @@
+// Js has 8-DT
+
+// function dt() {
+//     const length = 16;
+//     const weight = 7.5;
+//     const color = 'Yellow';
+//     const lastName = "Johnson";
+    
+//     const htmlContent = `
+//         <p>Length: ${length}</p>
+//         <p>Weight: ${weight}</p>
+//         <p>Color: ${color}</p>
+//         <p>Last Name: ${lastName}</p>
+//     `;
+
+//     document.getElementById('dt').innerHTML=htmlContent;
+// }
+// dt()
+
+
+// console.log(typeof (16 + 'volvo')); //js treat num as a str.
+
+// Js types are dynamic, means same variable can hold different data.
+// let x;
+// x=5;
+// x='John'; 
+
+// let ans = "It's alright 'John'";
+// console.log(ans);
+
+
+// Numbers ->Javascript numbers are always one type:double (64-bit floating point).
+// const num = 54;
+// console.log(num);
+
+// BigInt ->store int, which is too big .(o/p ends with 'n')
+// let bigNum = BigInt("12345678900987654321567438");
+// console.log(bigNum);
+
+// Booleans ->true or false
+// let x = 5;
+// let y = 5;
+// let z = 6;
+// console.log(x==y);
+// console.log(y==z);
+
+// Arrays - index starts from zero.
+// const cars = ['saab', 'volvo', 'BMW'];
+// console.log(cars[2]);
+
+// Objects
+// const person ={
+//     firstName : "John",
+//     last_name : "Doe",
+//     age : 50,
+//     eyeColor:"blue"
+// };
+// console.log(person.eyeColor);
